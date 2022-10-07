30cdd035a15c476b89f5959c462edd96578b0f78
juliozulu2008 <juliozulu2008@gmail.com>
2022-10-07 09:39:23 -0300

    array map slice

---
+++ funcoes/basicas/basicas.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func f1() { // funcao simples sem parametros sem retorno
+	fmt.Println("F1")
+}
+
+func f2(p1 string, p2 string) {
+	fmt.Printf("F2: %s %s\n", p1, p2)
+}
+
+func f3() {
+
+}
+
+func main() {
+
+}
